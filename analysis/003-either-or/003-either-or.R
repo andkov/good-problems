62c21fef69e2f38d5afb7b026866da696c7a2e0e
Andriy V. Koval <koval.andrey@gmail.com>
2022-02-02 14:46:03 -0700

    Create 003-either-or.R

---
+++ analysis/003-either-or/003-either-or.R
@@ -0,0 +1,60 @@
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+cat("\014") # Clear the console
+
+# verify root location
+cat("Working directory: ", getwd()) # Must be set to Project Directory
+# if the line above DOES NOT generates the project root, re-map by selecting
+# Session --> Set Working Directory --> To Project Directory location
+# Project Directory should be the root by default unless overwritten
+
+# ---- load-sources ------------------------------------------------------------
+
+source("./scripts/common-functions.R")
+
+# ---- load-packages -----------------------------------------------------------
+library(magrittr)  # pipes
+library(dplyr)     # data wrangling
+library(ggplot2)   # graphs
+library(janitor)   # tidy data
+library(tidyr)     # data wrangling
+library(forcats)   # factors
+library(stringr)   # strings
+library(lubridate) # dates
+
+# ---- declare-globals ---------------------------------------------------------
+
+# ---- declare-functions -------------------------------------------------------
+
+prints_folder <- paste0("./analysis/.../prints/")
+if(!file.exists(prints_folder)){
+  dir.create(file.path(prints_folder))
+}
+
+# ---- load-data ---------------------------------------------------------------
+
+
+# ---- inspect-data ------------------------------------------------------------
+
+
+# ---- tweak-data --------------------------------------------------------------
+
+
+# ---- table-1 -----------------------------------------------------------------
+
+
+# ---- graph-1 -----------------------------------------------------------------
+
+
+# ---- graph-2 -----------------------------------------------------------------
+
+# ---- save-to-disk ------------------------------------------------------------
+path <- "./analysis/.../report-isolated.Rmd"
+rmarkdown::render(
+  input = path ,
+  output_format=c(
+    "html_document"
+    # "word_document"
+    # "pdf_document"
+  ),
+  clean=TRUE
+)
