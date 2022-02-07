a7d7180aea239cc1254843283c7328780d0e5db7
Andriy V. Koval <koval.andrey@gmail.com>
2022-02-07 08:34:23 -0700

    stable

--- analysis/004-dual-metric/004-dual-metric-ALT.R
+++ analysis/004-dual-metric/004-dual-metric-ALT.R
@@ -34,22 +34,22 @@ library(lubridate) # dates
 b4_responses <- c("1" = "yes", "2" = "no", "-7" = "Q not asked")
 # If "yes", then asked "What fruit did you eat with your lunch?"
 # recording responses into two items (`b4_response1`,`b4_response2`)
-# each with the following possible values: 
+# each with the following possible values:
 fruit <- c("1" = "apple", "2" = "banana", "3" = "kiwi", "97" = "other")
 # Each time a fruit was named, follow up with question
-# "How much did you like it?" 
+# "How much did you like it?"
 b4_ratings <- c("1"="Very Bad", "2"="Bad","3"="Neutral", "4"="Good","5"="Very Good")
 # the following data set stores a sample of responses
 ds0 <-
   tibble::tribble(
     ~id, ~b4, ~b4_response1, ~b4_response2, ~b4_rating1, ~b4_rating2,
-    1, 1 ,  1,  2   , 1 , 4 ,       
+    1, 1 ,  1,  2   , 1 , 4 ,      
     2, 2 , NA, NA   ,NA ,NA ,      
     3, 1 ,  2, 3    , 3 , 4 ,      
     4, 1 ,  97, -7  ,1 ,  NA,      
     5, 1 ,  3, 97   , 3 , 5 ,      
     6, 1 ,  NA, 2   ,NA , 4 ,      
-    7, -7, -7, -7   ,NA , NA,   
+    7, -7, -7, -7   ,NA , NA,  
     8, 1 ,  97, 97  , 2 , 4 ,      
   ) %>%
   mutate_all(as.integer)
@@ -60,60 +60,76 @@ valid_responses <- c(1,2,3,97)
 # what percent of people had apple/banana/kiwi for lunch?
 # Step 1: Isolate variables to be evaluated
 (ds1 <- ds0 %>% select(id, b4_response1, b4_response2, b4_rating1, b4_rating2))
-# define the pool of valid values (the rest  will be converted to NA)
-valid_responses <- c(1,2,3,97)
-item_names <- c("b4_response1","b4_response2","b4_rating1", "b4_rating2")
+
 ds2 <-
   ds1 %>%
-  tidyr::pivot_longer(cols = item_names) %>%
+  tidyr::pivot_longer(-id ) %>%
   mutate(
-    value_valid = value %in% valid_responses # TRUE if target, FALSE if valid, but not target
-    ,item_response = paste0("b4","_",value)
+    item_type = case_when(
+      name %in% c("b4_response1","b4_response2") ~ "response"
+      ,name %in% c("b4_rating1","b4_rating2") ~ "rating"
+      )
+    ,item_order = case_when(
+      name %in% c("b4_response1","b4_rating1") ~ 1
+      ,name %in% c("b4_response2","b4_rating2") ~ 2
+      # TODO: figure out a way to automate for list of N pairs
+    )
   ) %>%
-  group_by(id, item_response) %>%
-  ungroup()
-valid_responses
+  select(-name) %>%
+  pivot_wider(names_from = "item_type", values_from = "value")
+ds1
 ds2
-
-# if any of the items contain a valid response -> case is valid
-# d_valid <- 
-#   ds2 %>% 
-#   # mutate(name = "b4") %>% 
-#   select(id, value_valid) %>% 
-#   group_by(id) %>% 
-#   summarize(
-#     value_valid = sum(value_valid, na.rm = T)>0L
-#   ) %>% 
-#   mutate(
-#     name = "b4_valid"
-#   ) %>% 
-#   pivot_wider(names_from = "name", values_from = "value_valid")
-# d_valid
-
-# make wide ds with indicators
+# browser()
 ds3 <-
   ds2 %>%
   mutate(
-    value = ifelse(value %in% valid_responses, value, NA)
-  ) %>% 
-  filter(!is.na(value)) %>%
-  select(id, item_response, value_valid) %>%
-  pivot_wider(names_from = item_response, values_from = value_valid, values_fn=max) %>%
-  mutate_at(
-    .vars = setdiff(names(.),"id")
-    ,.funs = as.logical
+    response = case_when(response %in% c(1,2,3,97) ~ response)
+    ,rating  =  case_when(rating %in% c(1,2,3,4,5) ~ rating)
+    # ,response_valid = case_when(response %in% valid_responses ~ TRUE,TRUE ~ FALSE)
+  ) %>%
+  # remove if provided save value for all responses
+  group_by(id) %>%
+  mutate(
+    same_responses = n_distinct(response)==1L
+  ) %>%
+  ungroup() %>%
+  filter(!same_responses) %>%  # b/c responses from two slots should be different
+  filter(!is.na(response)) %>% # so that pivoting produced proper results
+  mutate(
+    item_response = paste0("b4_",response)
   )
 ds3
-# join to the ds with raw responses
+
+ds3_response <-
+  ds3 %>%
+  select(id, item_response, response) %>%
+  mutate(
+    # name = paste0(item_response, "_response") # to make self-explanatory
+    name = paste0(item_response, "_a")          # to help view in console
+  ) %>%
+  select(-item_response) %>%
+  pivot_wider(names_from = "name", values_from = "response",
+              values_fn=is.integer, values_fill = FALSE)
+ds3_response 
+ds3_rating <-
+  ds3 %>%
+  select(id, item_response, rating) %>%
+  mutate(
+    # name = paste0(item_response, "_rating") # to make self-explanatory
+    name = paste0(item_response, "_b")        # to help view in console      
+  ) %>%
+  select(-item_response) %>%
+  pivot_wider(names_from = "name", values_from = "rating", values_fill = -99)
+ds3_rating
 
 ds4 <-
-  dplyr::left_join(
-    ds1
-    ,d_valid
+  ds1 %>%
+  left_join(
+    ds3_response
     ,by = "id"
-  ) %>% 
-  dplyr::left_join(
-    ds3
+  ) %>%
+  left_join(
+    ds3_rating
     ,by = "id"
   )
 ds4
@@ -124,6 +140,7 @@ make_indicator_responses_ratings <- function(
   d
   ,item_names
   ,valid_responses
+  ,valid_ratings
   ,id_name
   ,item_stem
   ,separator = "_"
@@ -135,7 +152,7 @@ make_indicator_responses_ratings <- function(
   # id_name = "id"
   # item_stem = "b4"
   # separator = "_"
-  
+
   # compute values to be used in operations
   all_relevant_vars <- c(id_name, unlist(item_names))
   d1 <- d %>% select(all_relevant_vars)
@@ -150,10 +167,10 @@ make_indicator_responses_ratings <- function(
   rating_names
   
   (d1 <- d %>% select( all_relevant_vars))
-
+  
   d2 <-
     d1 %>%
-    tidyr::pivot_longer(-c(id_name) ) %>% 
+    tidyr::pivot_longer(-c(id_name) ) %>%
     mutate(
       item_type = case_when(
         # name %in% c("b4_response1","b4_response2") ~ "response"
@@ -169,25 +186,25 @@ make_indicator_responses_ratings <- function(
         # add more lines if more that two pairs
         # TODO: figure out a way to automate for list of N pairs
       )
-    ) %>% 
+    ) %>%
     select(-name) %>%
     pivot_wider(names_from = "item_type", values_from = "value")
   d1
   d2
-  
-  d3 <- 
-    d2 %>% 
+  # browser()
+  d3 <-
+    d2 %>%
     mutate(
       response = case_when(response %in% valid_responses ~ response)
       ,rating  =  case_when(rating %in% valid_ratings ~ rating)
       # ,response_valid = case_when(response %in% valid_responses ~ TRUE,TRUE ~ FALSE)
-    ) %>% 
+    ) %>%
     # remove if provided save value for all responses
-    group_by(!!rlang::sym(id_name)) %>% 
+    group_by(!!rlang::sym(id_name)) %>%
     mutate(
       same_responses = n_distinct(response)==1L
-    ) %>% 
-    ungroup() %>% 
+    ) %>%
+    ungroup() %>%
     filter(!same_responses) %>%  # the order of filer is important!
     filter(!is.na(response)) %>% # the order of filer is important!
     mutate(
@@ -198,12 +215,12 @@ make_indicator_responses_ratings <- function(
       # )
       # ,item_response = paste0(item_stem,separator,response_valid)
       item_response = paste0(item_stem,separator,response)
-    ) 
+    )
   d3
   
-  d3_response <- 
-    d3 %>% 
-    select(c(id_name, item_response, response)) %>% 
+  d3_response <-
+    d3 %>%
+    select(c(id_name, item_response, response)) %>%
     mutate(
       # name = paste0(item_response, "_response")
       name = paste0(item_response, "_a")
@@ -211,56 +228,31 @@ make_indicator_responses_ratings <- function(
     select(-item_response) %>%
     pivot_wider(names_from = "name", values_from = "response",
                 values_fn=is.integer, values_fill = FALSE)
- d3_response #%>% View()
- d3_rating <- 
-   d3 %>% 
-   select(c(id_name, item_response, rating)) %>% 
-   mutate(
-     # name = paste0(item_response, "_rating")
-     name = paste0(item_response, "_b")
-   ) %>% 
-    select(-item_response) %>% 
-   pivot_wider(names_from = "name", values_from = "rating", values_fill = -99)
- d3_rating 
- 
- d4 <- 
-   d1 %>% 
-   left_join(
-     d3_response
-     ,by = id_name
-   ) %>% 
-   left_join(
-     d3_rating
-     ,by = id_name
-   )
-  d4
-  
-  # make wide ds with indicators
-  d3 <-
-    d2 %>%
+  d3_response #%>% View()
+  d3_rating <-
+    d3 %>%
+    select(c(id_name, item_response, rating)) %>%
     mutate(
-      value = ifelse(value %in% valid_responses, value, NA)
-    ) %>% 
-    filter(!is.na(value)) %>%
-    select(!!!rlang::syms(c(id_name, "item_response", "value_valid"))) %>% 
-    pivot_wider(names_from = item_response, values_from = value_valid, values_fn=max,values_fill = FALSE) %>%
-    mutate_at(
-      .vars = setdiff(names(.),id_name)
-      ,.funs = as.logical
-    )
-  d3
-  
-  
-  
-  # # join to the ds with raw responses
+      # name = paste0(item_response, "_rating")
+      name = paste0(item_response, "_b")
+    ) %>%
+    select(-item_response) %>%
+    pivot_wider(names_from = "name", values_from = "rating", values_fill = -99)
+  d3_rating
   
   d4 <-
-    dplyr::left_join(
-      d
-      ,d3
+    d1 %>%
+    left_join(
+      d3_response
       ,by = id_name
-    ) 
+    ) %>%
+    left_join(
+      d3_rating
+      ,by = id_name
+    )
   d4
+  
+  #
   
   return(d4)
   
@@ -268,21 +260,17 @@ make_indicator_responses_ratings <- function(
 
 # how to use
 ds0
-ds0 %>% 
+ds0 %>%
   make_indicator_responses_ratings(
-    item_names = c("b4_response1", "b4_response2")
+    item_names = list(c("b4_response1","b4_rating1"),c("b4_response2","b4_rating2"))
     ,valid_responses = c(1,2,3,97)
+    ,valid_ratings   = c(1,2,3,4,5)
     ,id_name = "id"
     ,item_stem = "b4"
     ,separator = "_"
-  ) %>% 
-  make_indicator_responses_ratings(
-    item_names = c("b4_rating1", "b4_rating2")
-    ,valid_responses = c(1,2,3,4,5)
-    ,id_name = "id"
-    ,item_stem = "b4r"
-    ,separator = "_"
   )
+
+
 
 # ---- inspect-data ------------------------------------------------------------
 
